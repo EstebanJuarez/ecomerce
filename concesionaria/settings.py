5ef177da003a9929f0df26564fe110f45ed15b93
EstebanJuarez <esteban.juarez0011@gmail.com>
2022-11-06 16:29:03 -0300

    7r commit

--- concesionaria/settings.py
+++ concesionaria/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
 BASE_DIR/ "static"
 ]
 
-STATIC_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATIC_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
